0e702de418c22ec97890ae7780136a70138a3788
xalidevxx <xalidevxx@gmail.com>
2024-11-29 19:39:07 +0500

    code review

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    // Ссылки на кнопки
-    const appStoreButton = document.getElementById("appStoreButton");
-    const playMarketButton = document.getElementById("playMarketButton");
-    const websiteButton = document.getElementById("websiteButton");
-
-    // Логика проверки системы
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        // Устройство на iOS
-        appStoreButton.style.display = "flex";
-    } else if (/android/i.test(userAgent)) {
-        // Устройство на Android
-        playMarketButton.style.display = "flex";
-    } else {
-        // Для других устройств
-        websiteButton.style.display = "flex";
-    }
+document.addEventListener('DOMContentLoaded', function () {
+	const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	const appStoreButton = document.getElementById('appStoreButton');
+	const playMarketButton = document.getElementById('playMarketButton');
+	const websiteButton = document.getElementById('websiteButton');
+	if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+		appStoreButton.style.display = 'flex';
+	} else if (/android/i.test(userAgent)) {
+		playMarketButton.style.display = 'flex';
+	} else {
+		websiteButton.style.display = 'flex';
+	}
 });
