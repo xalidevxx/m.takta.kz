41a53fb83e4891d1bfe7dae4014972fadaae92ef
Alikhan <xalidevxx@gmail.com>
2025-01-09 23:54:55 +0500

    fix: platforms

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
 	const appStoreButton = document.getElementById('appStoreButton');
-	const playMarketButton = document.getElementById('playMarketButton');
-	const websiteButton = document.getElementById('websiteButton');
+	const androidButton = document.getElementById('androidButton');
+	const webButton = document.getElementById('webButton');
+
+	const iosImg = document.getElementById('iosImg');
+	const androidImg = document.getElementById('androidImg');
+	const webImg = document.getElementById('webImg');
+
 	if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
 		appStoreButton.style.display = 'flex';
+		iosImg.style.display = 'block';
 	} else if (/android/i.test(userAgent)) {
-		playMarketButton.style.display = 'flex';
+		androidButton.style.display = 'flex';
+		androidImg.style.display = 'block';
 	} else {
-		websiteButton.style.display = 'flex';
+		webButton.style.display = 'flex';
+		webImg.style.display = 'block';
 	}
 });
