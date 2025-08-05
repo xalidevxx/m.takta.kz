8040f1f8e05f3a7e710bd735d57779a428b59ec0
Alikhan <xalidevxx@gmail.com>
2025-08-06 01:06:26 +0500

    fix: update design

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-	const isIos = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+	// const isIos = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
 	const isAndroid = /android/i.test(userAgent);
 	const appBtn = document.getElementById('app-btn');
-	if (isIos) {
-		appBtn.href = 'https://apps.apple.com/kz/app/takta-kz/id6504254858';
-	} else if (isAndroid) {
+	if (isAndroid) {
 		appBtn.href =
 			'https://play.google.com/store/apps/details?id=kz.takta.takta';
-	} else {
-		window.location.href = 'https://takta.kz';
 	}
 });
